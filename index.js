ca111152100e8fda12d156a1726fdba74779ebd5
Anthony <kabuthuantony@gmail.com>
2024-04-09 13:45:58 +0300

    Fetch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+async function submitData(name, email) {
+    try {
+      const response = await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email
+        })
+      });
+  
+      if (!response.ok) {
+        throw new Error('Request failed');
+      }
+  
+      const responseData = await response.json();
+      document.body.innerHTML += responseData.id;
+    } catch (error) {
+      document.body.innerHTML += error.message;
+    }
+  }
+  
--- test/helpers.js
+++ test/helpers.js
@@ -20,3 +20,33 @@ jsdom( {
   src,
   url: "http://localhost"
 } );
+// helpers.js
+const fetch = require('node-fetch');
+
+async function submitData(name, email) {
+  try {
+    const response = await fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ name, email })
+    });
+    
+    const responseData = await response.json();
+
+    if (response.ok) {
+      document.body.innerHTML += responseData.id;
+    } else {
+      document.body.innerHTML += responseData.message;
+    }
+  } catch (error) {
+    document.body.innerHTML += error.message;
+  }
+}
+
+module.exports = {
+  submitData: submitData
+};
+
